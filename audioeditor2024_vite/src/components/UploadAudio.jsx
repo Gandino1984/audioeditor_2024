2a1940d41aeaedbd4ae35cc3902eabd8e2fbba55
Gandino1984 <andinogerman@gmail.com>
2024-07-15 11:53:21 +0200

    file format comprobation

--- audioeditor2024_vite/src/components/UploadAudio.jsx
+++ audioeditor2024_vite/src/components/UploadAudio.jsx
@@ -8,6 +8,7 @@ export const UploadAudio = () => {
     const inputFile = useRef(null);
     const { setFileURL } = useContext(FileContext);
     const [file, setFile] = useState(null);
+    const [fileError, setFileError] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -31,17 +32,22 @@ export const UploadAudio = () => {
     const handleFileUpload = (e) => {
         const uploadedFile = e.target.files[0];
         if (uploadedFile) {
-            console.log('File selected:', uploadedFile.name);
-            setFile(uploadedFile);
+            const fileExtension = uploadedFile.name.split('.').pop().toLowerCase();
+            if (!['mp3', 'wav', 'ogg'].includes(fileExtension)) { // Add more supported extensions as needed
+                setFileError('Unsupported file format. Please upload an MP3, WAV, or OGG file.');
+            } else {
+                setFileError('');
+                console.log('File selected:', uploadedFile.name);
+                setFile(uploadedFile);
+            }
         }
     };
 
     return (
         <div className='upload-audio'>
-          
-            <h1>Upload your audio file: </h1>
+            <h1>Upload your audio file:</h1>
             <button className='upload-btn' onClick={handleButtonClick}>
-                Upload 
+                Upload
             </button>
             <input
                 type='file'
@@ -51,6 +57,7 @@ export const UploadAudio = () => {
                 accept='audio/*'
                 onChange={handleFileUpload}
             />
+            {fileError && <p className='error-message'><ion-icon name="warning-outline"></ion-icon> {fileError}</p>}
         </div>
     );
 };
